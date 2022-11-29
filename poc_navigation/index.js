3f584e6fff5d923686c61170d48626ae4618fda7
Rafaela de Moraes Papale <rafaela.papale@gec.inatel.br>
2022-11-29 09:56:58 -0300

    :construction: Corrigindo navDrawer

--- poc_navigation/index.js
+++ poc_navigation/index.js
@@ -1,7 +1,7 @@
 /**
  * @format
  */
-
+import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import Navegacao from './src/navigation';
 import { name as appName } from './app.json';
